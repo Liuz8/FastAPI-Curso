f5220b5112f84d670ded3bd09116b2d7cca947ae
Luis <luismarfran08@gmail.com>
2024-03-11 19:01:26 -0600

    nueva estructura de elementos de programa: Item
    nueva estructura de elementos de programa: Item

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel
+
 
 # creacion de una app fastapi
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+
 
 
 @app.get("/")
