115385f9dc2d5579a4de1790aec0a2a69178a4ad
Luis <luismarfran08@gmail.com>
2024-03-19 16:51:56 -0600

    Uso de clases con parametros
    No se hace caso tanto al video, ya que actualizaron la sintaxis y ahora el atributo name tiene que inicializarse con un str

---
+++ S04_types/S04_13_pydantic.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int 
+    name: str = 'John Doe'
+    signup_ts: datetime | None = None
+    friends: list[int] = []
+
+# Desde el exterior podríamos recibir estos datos:
+external_data = {
+    'id': 1001,
+    'signup_ts': '2022-10-23 12:20',
+    'friends': [1002, 1003, 1004]
+}
+
+user = User(**external_data)
+print(user)
+print(user.id)
