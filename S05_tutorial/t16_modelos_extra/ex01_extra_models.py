03e5cc64fe5ae7ad8c850df6eb162dc6620c4291
Luis <luismarfran08@gmail.com>
2024-05-08 23:25:19 -0600

    Impresiones para ver el contenido

--- S05_tutorial/t16_modelos_extra/ex01_extra_models.py
+++ S05_tutorial/t16_modelos_extra/ex01_extra_models.py
@@ -29,8 +29,10 @@ def fake_password_hasher(raw_password: str):
 
 def fake_save_user(user_in: UserIn):
     hashed_password = fake_password_hasher(user_in.password)
+    print(hashed_password)
     user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
     print("User saved! ..not really")
+    print(user_in_db)
     return user_in_db
 
 @app.post("/user/", response_model=UserOut)
